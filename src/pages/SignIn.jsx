9215b5f6ef28d6df91aa1b3bafe09013b1831115
Anzo256 <anzoangelo@gmail.com>
2022-10-22 16:19:02 -0700

    complete the sign in functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react"
 import {AiFillEyeInvisible,AiFillEye} from "react-icons/ai"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
+import {  useNavigate} from "react-router-dom"
 import OAuth from "../components/OAuth";
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+import { toast } from "react-toastify";
+
 
 
 export default function SignIn() {
@@ -11,12 +15,26 @@ export default function SignIn() {
     password:"",
   });
   const {email,password} = formData;
+  const navigate = useNavigate();
   function onChange(e){
    setFormData((prevState)=>({
     ...prevState,
-    [e.target.id]: e.target.vale,
-   }))
+    [e.target.id]: e.target.value,
+   }));
   }
+  async function onSubmit(e){
+    e.preventDefault()
+    try {
+      const auth = getAuth();
+      const userCredential = await signInWithEmailAndPassword(auth, email, password)
+    if (userCredential.user) {
+      navigate("/");
+    }
+    } catch (error) {
+     toast.error("Unverified user credentials!!!");
+     //console.log(error);
+    }
+}
   return (
     <section>
       <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
@@ -25,9 +43,13 @@ export default function SignIn() {
         <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=773&q=80" alt="key" className="w-full rounded-2xl"/>
       </div>
       <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-        <form>
-          <input  type="email" id="email" value={email} onChange={onChange} 
-          placeholder="Email address" className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"/>
+        <form onSubmit={onSubmit}>
+          <input  type="email"
+           id="email"
+           value={email}
+           onChange={onChange} 
+           placeholder="Email address"
+           className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"/>
           <div className="relative mb-6">
           <input  type={showPassword ? "text" : "password"} id="password" value={password} onChange={onChange} 
           placeholder="Password" className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"/>
