e152042a9562c84d7403c0c4fef7e7f7553e5f83
Anzo256 <anzoangelo@gmail.com>
2022-11-14 13:21:09 -0800

    Create Edit listing page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Header from "./components/Header";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import CreateListing from "./pages/CreateListing";
+import EditListing from "./pages/EditListing";
 function App() {
   return (
     <div>
@@ -29,7 +30,11 @@ function App() {
           <Route path="create-listing" element={<PrivateRoute/>}>
             <Route path="/create-listing" element={<CreateListing/>}/>
           </Route>
-          
+
+          <Route path="edit-listing" element={<PrivateRoute/>}>
+            <Route path="/edit-listing/:listingId" element={<EditListing/>}/>
+          </Route>
+
         </Routes>
       </Router>
       <ToastContainer
